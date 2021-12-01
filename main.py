39dcd4360e21838478fb4542e286cf6c3ccde46b
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2021-11-30 23:11:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from machine import Pin, ADC
 import utime
 import time
 
-
 sail_step = Pin(12)
 sail_direction = Pin(13, Pin.OUT)
 
@@ -13,9 +12,8 @@ lift_step = Pin(14)
 lift_direction = Pin(15, Pin.OUT)
 
 select_button = Pin(19, Pin.IN, Pin.PULL_DOWN)
-direction_button = Pin(18, Pin.IN, Pin.PULL_DOWN)
-
-
+forward_button = Pin(21, Pin.IN, Pin.PULL_DOWN)
+reverse_button = Pin(18, Pin.IN, Pin.PULL_DOWN)
 
 # To control speed just modify the amount/value of nop[dely amount 0-31].
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
@@ -41,9 +39,9 @@ lift_motor = rp2.StateMachine(1, move, freq=100000, set_base=lift_step)
 claw_motor = rp2.StateMachine(2, move, freq=100000, set_base=claw_step)
 
 # select = [sail, lift, claw] = [0,1,2]
-select = 1
+select = 0
 # direction = [off, forward, backward] = [0,1,2]
-direction = 1
+direction = 0
 
 while True:
     
@@ -51,27 +49,31 @@ while True:
         if select == 0:
             """Sail"""
             select = 1
+            print("Sail")
             
         elif select == 1:
             """Lift"""
             select = 2
+            print("Lift")
             
         elif select == 2:
             """Claw"""
             select = 0
+            print("Claw")
             
         # Required so the button has time to reset.    
         time.sleep(0.5)
     
-    if direction_button.value():
+
+    if forward_button.value():
         if direction == 0:
             """Dont move"""
             sail_motor.active(0)
             lift_motor.active(0)
             claw_motor.active(0)
             direction = 1
-            print(0)
-              
+            print("All off")
+
         elif direction == 1:
             """forward"""
             # 1 In / 0 Out
@@ -97,8 +99,17 @@ while True:
             
             # Set next click
             direction = 2
-            print(2)
-            
+            print("Forward")
+
+
+    if reverse_button.value():
+        if direction == 0:
+            """Dont move"""
+            sail_motor.active(0)
+            lift_motor.active(0)
+            claw_motor.active(0)
+            direction = 2
+            print("All off")
         elif direction == 2:
             """Backward"""
             sail_direction.value(1)
@@ -114,15 +125,8 @@ while True:
             elif select == 2:
                 """Claw"""
                 claw_motor.active(1)
-            direction = 0
-            
-            print(2)
-        
-        
-        
-            
+            direction = 0      
+            print("Backward")
+
         # Required so the button has time to reset.
         time.sleep(0.5)
-
-
-
